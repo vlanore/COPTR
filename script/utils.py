35577f33405f86f391402aab867580a431cfbc58
Vincent Lanore <vincent.lanore@univ-lyon1.fr>
2018-02-15 17:58:16 +0100

    More work on convergence script + new utils.py file for latest utils (to be merged with old utils at some point).

---
+++ script/utils.py
@@ -0,0 +1,45 @@
+import pandas as pd
+from docopt import docopt
+from diffsel_script_utils import param, data
+import diffsel_script_utils as utils
+from subprocess import Popen, PIPE, call
+from os.path import isfile
+from os import remove
+
+def strip(s):
+    if s[0] == '-' or s[0] == '<':
+        return strip(s[1:])
+    elif s[-1:] == '>':
+        return s[:-1]
+    else:
+        return s
+
+def to_str(thing):
+    if isinstance(thing, int):
+        return str(thing)
+    elif not isinstance(thing, str):
+        return str(thing[0])
+    else:
+        return str(thing)
+
+def STEP(s):
+    print(utils.step(s))
+
+def INFO(s):
+    print("-- "+s)
+
+def ERROR(s):
+    print("\n\t["+utils.boldred("Error")+"] "+s)
+
+def BAD(s):
+    print(utils.bad(s))
+
+def GOOD(s):
+    print(utils.good(s))
+
+def getargs(s):
+    args = docopt(s)
+    for arg in args:
+        if arg != "--help":
+            INFO(strip(arg)+" set to "+param(to_str(args[arg])))
+    return args
